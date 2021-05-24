464c15683200870dc6f34f507b5ee226dd9e9d55
gilberto <gilberto.vento@gmail.com>
2021-05-24 07:04:55 -0400

    Adding least recently used cache.

--- src/main/java/com/github/gilbva/cspractice/datastructures2/LRUCache.java
+++ src/main/java/com/github/gilbva/cspractice/datastructures2/LRUCache.java
@@ -5,14 +5,15 @@ public class LRUCache<K, V> {
     public LRUCache(int capacity) {
     }
 
-    public void clear() {
-    }
-
     public V get(K key) {
         throw new UnsupportedOperationException();
     }
 
     public void refer(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
 
+    public void clear() {
+        throw new UnsupportedOperationException();
     }
 }
