9b66287bbff49615657b1a76569909b3dcdf281b
gilberto <gilberto.vento@gmail.com>
2021-05-24 06:52:13 -0400

    Adding least recently used cache.

---
+++ src/main/java/com/github/gilbva/cspractice/datastructures2/LRUCache.java
@@ -0,0 +1,18 @@
+package com.github.gilbva.cspractice.datastructures2;
+
+public class LRUCache<K, V> {
+
+    public LRUCache(int capacity) {
+    }
+
+    public void clear() {
+    }
+
+    public V get(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void refer(K key, V value) {
+
+    }
+}
--- src/test/java/com/github/gilbva/cspractice/DataStructures2Test.java
+++ src/test/java/com/github/gilbva/cspractice/DataStructures2Test.java
@@ -1,6 +1,7 @@
 package com.github.gilbva.cspractice;
 
 import com.github.gilbva.cspractice.datastructures2.HashMap;
+import com.github.gilbva.cspractice.datastructures2.LRUCache;
 import com.github.gilbva.cspractice.datastructures2.PriorityQueue;
 import com.github.gilbva.cspractice.datastructures2.TreeMap;
 import org.junit.jupiter.api.Assertions;
@@ -16,7 +17,7 @@ public class DataStructures2Test {
         IntArrayCallback callback = (arr) -> {
             String[] keys = new String[arr.length];
             HashMap<String, Integer> map = new HashMap<>();
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(i, map.size());
                 keys[i] = UUID.randomUUID().toString();
                 map.put(keys[i], arr[i]);
@@ -25,7 +26,7 @@ public class DataStructures2Test {
                 Assertions.assertEquals(map.get(keys[i]), arr[i]);
             }
             Assertions.assertEquals(arr.length, map.size());
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(arr.length - i, map.size());
                 Assertions.assertTrue(map.exists(keys[i]));
                 Assertions.assertEquals(arr[i], map.get(keys[i]));
@@ -42,10 +43,10 @@ public class DataStructures2Test {
             }
             Assertions.assertEquals(0, map.size());
         };
-        for(int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             result.add(TestUtils.genericArrayTest(i, "test hash map " + i, callback));
         }
-        for(int i = 100; i < 10_000; i+=500) {
+        for (int i = 100; i < 10_000; i += 500) {
             result.add(TestUtils.genericArrayTest(i, "test hash map " + i, callback));
         }
         return result;
@@ -56,12 +57,12 @@ public class DataStructures2Test {
         List<DynamicTest> result = new ArrayList<>();
         IntArrayCallback callback = (arr) -> {
             PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a));
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(i, queue.size());
                 queue.offer(arr[i]);
             }
             Assertions.assertEquals(arr.length, queue.size());
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(arr.length - i, queue.size());
                 int value = queue.peek();
                 arr[i] = queue.poll();
@@ -69,10 +70,10 @@ public class DataStructures2Test {
             }
             Assertions.assertEquals(0, queue.size());
         };
-        for(int i = 0; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             result.add(TestUtils.arraySortTest(i, "test priority queue sort " + i, callback));
         }
-        for(int i = 1_000; i < 100_000; i+=1_000) {
+        for (int i = 1_000; i < 100_000; i += 1_000) {
             result.add(TestUtils.arraySortTest(i, "test priority queue sort " + i, callback));
         }
         return result;
@@ -84,7 +85,7 @@ public class DataStructures2Test {
         IntArrayCallback callback = (arr) -> {
             String[] keys = new String[arr.length];
             TreeMap<String, Integer> map = new TreeMap<>(String::compareTo);
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(i, map.size());
                 keys[i] = UUID.randomUUID().toString();
                 map.put(keys[i], arr[i]);
@@ -100,12 +101,12 @@ public class DataStructures2Test {
                 Assertions.assertTrue(map.exists(keys[i]));
                 Assertions.assertEquals(map.get(keys[i]), arr[i]);
 
-                for(String key : map) {
+                for (String key : map) {
                     Assertions.assertNotNull(key);
                 }
             }
             Assertions.assertEquals(arr.length, map.size());
-            for(int i = 0; i < arr.length; i++) {
+            for (int i = 0; i < arr.length; i++) {
                 Assertions.assertEquals(arr.length - i, map.size());
                 Assertions.assertTrue(map.exists(keys[i]));
                 Assertions.assertEquals(arr[i], map.get(keys[i]));
@@ -120,18 +121,88 @@ public class DataStructures2Test {
                 Assertions.assertFalse(map.exists(keys[i]));
                 Assertions.assertNull(map.get(keys[i]));
 
-                for(String key : map) {
+                for (String key : map) {
                     Assertions.assertNotNull(key);
                 }
             }
             Assertions.assertEquals(0, map.size());
         };
-        for(int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             result.add(TestUtils.genericArrayTest(i, "test tree map " + i, callback));
         }
-        for(int i = 100; i < 10_000; i+=500) {
+        for (int i = 100; i < 10_000; i += 500) {
             result.add(TestUtils.genericArrayTest(i, "test tree map " + i, callback));
         }
+        return result;
+    }
+
+    @TestFactory
+    Collection<DynamicTest> testLRUCache() {
+        List<DynamicTest> result = new ArrayList<>();
+        result.add(DynamicTest.dynamicTest("test lru cache 1", () -> {
+            LRUCache<String, Integer> cache = new LRUCache<>(5);
+            cache.refer("one", 1);
+            Assertions.assertNotNull(cache.get("one"));
+            cache.refer("two", 2);
+            Assertions.assertNotNull(cache.get("two"));
+            cache.refer("three", 3);
+            Assertions.assertNotNull(cache.get("three"));
+            cache.refer("four", 4);
+            Assertions.assertNotNull(cache.get("four"));
+            cache.refer("five", 5);
+            Assertions.assertNotNull(cache.get("five"));
+            cache.refer("six", 6);
+            Assertions.assertNotNull(cache.get("six"));
+            Assertions.assertNull(cache.get("one"));
+            Assertions.assertNotNull(cache.get("two"));
+            Assertions.assertNotNull(cache.get("three"));
+            Assertions.assertNotNull(cache.get("four"));
+            Assertions.assertNotNull(cache.get("five"));
+            Assertions.assertNotNull(cache.get("six"));
+            cache.refer("seven", 7);
+            Assertions.assertNotNull(cache.get("seven"));
+            Assertions.assertNull(cache.get("two"));
+            Assertions.assertNotNull(cache.get("three"));
+            cache.refer("eight", 8);
+            Assertions.assertNull(cache.get("four"));
+            Assertions.assertNotNull(cache.get("three"));
+
+            cache.clear();
+            Assertions.assertNull(cache.get("one"));
+            Assertions.assertNull(cache.get("two"));
+            Assertions.assertNull(cache.get("three"));
+            Assertions.assertNull(cache.get("four"));
+            Assertions.assertNull(cache.get("five"));
+            Assertions.assertNull(cache.get("six"));
+            Assertions.assertNull(cache.get("seven"));
+            Assertions.assertNull(cache.get("eight"));
+
+            cache.refer("one", 1);
+            Assertions.assertNotNull(cache.get("one"));
+            cache.refer("two", 2);
+            Assertions.assertNotNull(cache.get("two"));
+            cache.refer("three", 3);
+            Assertions.assertNotNull(cache.get("three"));
+            cache.refer("four", 4);
+            Assertions.assertNotNull(cache.get("four"));
+            cache.refer("five", 5);
+            Assertions.assertNotNull(cache.get("five"));
+            cache.refer("six", 6);
+            Assertions.assertNotNull(cache.get("six"));
+            Assertions.assertNull(cache.get("one"));
+            Assertions.assertNotNull(cache.get("two"));
+            Assertions.assertNotNull(cache.get("three"));
+            Assertions.assertNotNull(cache.get("four"));
+            Assertions.assertNotNull(cache.get("five"));
+            Assertions.assertNotNull(cache.get("six"));
+            cache.refer("seven", 7);
+            Assertions.assertNotNull(cache.get("seven"));
+            Assertions.assertNull(cache.get("two"));
+            Assertions.assertNotNull(cache.get("three"));
+            cache.refer("eight", 8);
+            Assertions.assertNull(cache.get("four"));
+            Assertions.assertNotNull(cache.get("three"));
+        }));
         return result;
     }
 }
