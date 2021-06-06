5b388d399914f68621a26e6f30bd6a24da593af1
gilberto <gilberto.vento@gmail.com>
2021-06-06 17:16:26 -0400

    Adding btree.

---
+++ src/main/java/com/github/gilbva/cspractice/datastructures2/BTree.java
@@ -0,0 +1,13 @@
+package com.github.gilbva.cspractice.datastructures2;
+
+public class BTree {
+    public static final int MD = 2;
+
+    public static class Page {
+
+    }
+
+    public void splitChild(Page parent, int index) {
+
+    }
+}
