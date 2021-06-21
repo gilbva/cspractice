ec765c6d9688e86b99fb544e2df469fa18f7bbea
gilberto <gilberto.vento@gmail.com>
2021-06-21 08:39:27 -0400

    Implementing sorting algorithms.

---
+++ src/main/java/com/github/gilbva/cspractice/sorting/SortUtils.java
@@ -0,0 +1,9 @@
+package com.github.gilbva.cspractice.sorting;
+
+public class SortUtils {
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
