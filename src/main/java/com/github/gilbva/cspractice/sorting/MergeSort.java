9510095f808c66955a5ea6d372fa7656e9a3896a
gilberto <gilberto.vento@gmail.com>
2021-07-02 08:47:04 -0400

    Fix in sorting algorithms

--- src/main/java/com/github/gilbva/cspractice/sorting/MergeSort.java
+++ src/main/java/com/github/gilbva/cspractice/sorting/MergeSort.java
@@ -4,4 +4,12 @@ public class MergeSort {
     public static void mergeSort(int[] arr) {
 
     }
+
+    private static void merge(int[] arr, int p, int q, int r) {
+
+    }
+
+    private static void mergeReduced(int[] arr, int p, int q, int r) {
+
+    }
 }
--- src/main/java/com/github/gilbva/cspractice/sorting/MergeSortInPlace.java
+++
@@ -1,7 +0,0 @@
-package com.github.gilbva.cspractice.sorting;
-
-public class MergeSortInPlace {
-    public static void mergeSortInPlace(int[] arr1, int[] arr2) {
-
-    }
-}
