4121652b4061102f47d2a84c132e9ce8d8cc79e1
gilberto <gilberto.vento@gmail.com>
2022-07-26 19:39:22 -0400

    Adding couchbase.

---
+++ src/main/java/com/github/gilbva/cspractice/cloudcomputing/dataservers/CouchbasePayments.java
@@ -0,0 +1,9 @@
+package com.github.gilbva.cspractice.cloudcomputing.dataservers;
+
+import com.github.gilbva.cspractice.cloudcomputing.dataservers.model.PaymentTransaction;
+
+public class CouchbasePayments {
+    public void add(PaymentTransaction transaction) {
+
+    }
+}
---
+++ src/main/java/com/github/gilbva/cspractice/cloudcomputing/dataservers/model/PaymentTransaction.java
@@ -0,0 +1,46 @@
+package com.github.gilbva.cspractice.cloudcomputing.dataservers.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class PaymentTransaction {
+    private String id;
+
+    private String account;
+
+    private BigDecimal amount;
+
+    private LocalDateTime datetime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+}
