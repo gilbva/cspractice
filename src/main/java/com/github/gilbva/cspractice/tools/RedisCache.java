ff7924024cf21b80b9b5db475c78f7e244d7c35a
gilberto <gilberto.vento@gmail.com>
2022-07-26 16:22:28 -0400

    Implementando redis test.

---
+++ src/main/java/com/github/gilbva/cspractice/tools/RedisCache.java
@@ -0,0 +1,25 @@
+package com.github.gilbva.cspractice.tools;
+
+import java.util.Set;
+
+public class RedisCache {
+
+    public RedisCache(String host, int port) {
+    }
+
+    public Set<String> allKeys() {
+        return null;
+    }
+
+    public String get(String key) {
+        return null;
+    }
+
+    public void put(String key, String value, long expirationInSec) {
+
+    }
+
+    public void remove(String key) {
+
+    }
+}
