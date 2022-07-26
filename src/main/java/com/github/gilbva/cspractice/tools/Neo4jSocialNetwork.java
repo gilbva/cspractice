88a2f97a73efa9146fc5c55ba99578f412159983
gilberto <gilberto.vento@gmail.com>
2022-07-26 18:39:02 -0400

    Adding neo4j initial component

---
+++ src/main/java/com/github/gilbva/cspractice/tools/Neo4jSocialNetwork.java
@@ -0,0 +1,33 @@
+package com.github.gilbva.cspractice.tools;
+
+import org.neo4j.driver.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Neo4j Cypher https://neo4j.com/docs/cypher-manual/current/clauses
+ */
+public class Neo4jSocialNetwork implements AutoCloseable {
+
+    public Neo4jSocialNetwork(String url) {
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+
+    public List<Integer> getUsers() {
+        return null;
+    }
+
+    public void deleteAll() {
+    }
+
+    public void addUser(String name, int age) {
+    }
+
+    public Map<String, Integer> getUser(Integer id) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/gilbva/cspractice/tools/Neo4jSocialNetworkTest.java
@@ -0,0 +1,39 @@
+package com.github.gilbva.cspractice.tools;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Neo4jSocialNetworkTest {
+
+    private Random random = new Random();
+
+    @Test
+    public void testSocialNetwork() throws Exception {
+        try(var graph = new Neo4jSocialNetwork("bolt://neo4j:7687")) {
+            graph.deleteAll();
+            Assertions.assertTrue(graph.getUsers().isEmpty());
+
+            TreeMap<String, Integer> usersMap = new TreeMap<>();
+            for (int i = 0; i < 1000; i++) {
+                String name = UUID.randomUUID().toString().substring(0, 5);
+                int age = 3 + (int) (79d * random.nextGaussian());
+
+                usersMap.put(name, age);
+                graph.addUser(name, age);
+            }
+
+            List<Integer> ids = graph.getUsers();
+            var users = new TreeMap<>(ids.stream()
+                                                .map(graph::getUser)
+                                                .flatMap(x -> x.entrySet().stream())
+                                                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+
+            Assertions.assertArrayEquals(users.keySet().toArray(), usersMap.keySet().toArray());
+            Assertions.assertArrayEquals(users.values().toArray(), usersMap.values().toArray());
+
+        }
+    }
+}
